311232f5e7df8499c1a8c67f8ed6e5accde53dda
Peter Tiernan <ptiernan@gmail.com>
2019-01-20 23:14:50 +0000

    starting services page

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -7,5 +7,88 @@ Services | Rocket.Chip Web Solutions
 @endsection
 
 @section('content')
-    Testing
+    <div class="services-banner">
+        <h1>Our Services</h1>
+    </div>
+
+    <div class="services-header">
+        <h2><span>R</span>ocket.Chip Web Solutions</h2>
+        <p>All the tools you need to get your business online!</p>
+    </div>
+    <div class="svg-divider-services">
+        <img src="/images/divider2.svg" alt="">
+    </div>
+
+    <div class="services-summary-container">
+        <div class="services-summary">
+            <div class="services-summary-custom">
+                <i class="far fa-object-group"></i>
+                <h2>Custom Web Design</h2>
+            </div>
+            <div class="services-summary-responsive">
+                <i class="fas fa-mobile-alt"></i>
+                <h2>Responsive Design</h2>
+            </div>
+
+            <div class="services-summary-redesign">
+                <i class="fas fa-laptop"></i>
+                <h2>Website Redesign</h2>
+            </div>
+            <div class="services-summary-maintenance">
+                <i class="fas fa-users-cog"></i>
+                <h2>Website Maintenance</h2>
+            </div>
+            <div class="services-summary-seo">
+                <i class="fas fa-search"></i>
+                <h2>Search Engine Optimisation</h2>
+            </div>
+            <div class="services-summary-graphic">
+                <i class="fas fa-pen-fancy"></i>
+                <h2>Graphic Design</h2>
+            </div>
+
+        </div>
+    </div>
+    <div class="design-process-container">
+        <div class="design-process">
+            <div class="design-process-header">
+                <h1>Our Design Process</h1>
+                <p>Your website is an important part of your business. It is an essential element in attracting new customers and letting existing customers know what you offer. We understand this and, through our design process, we help to ensure your website acts as an effective marketing and communications tool.</p>
+            </div>
+            <div class="design-process-1">
+                <div class="left">
+                    Testing
+                </div>
+                <div class="right">
+                    Testing
+                </div>
+            </div>
+            <div class="design-process-2">
+                <div class="left">
+                    Testing
+                </div>
+                <div class="right">
+                    Testing
+                </div>
+            </div>
+            <div class="design-process-3">
+                <div class="left">
+                    Testing
+                </div>
+                <div class="right">
+                    Testing
+                </div>
+            </div>
+            <div class="design-process-4">
+                <div class="left">
+                    Testing
+                </div>
+                <div class="right">
+                    Testing
+                </div>
+            </div>
+
+        </div>
+    </div>
+
 @endsection
