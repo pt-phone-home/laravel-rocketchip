192fedaa1830ba640a3a1d36a8377abe6448cec4
Peter Tiernan <ptiernan@gmail.com>
2019-02-09 13:37:14 +0000

    updating portfolio page

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -18,7 +18,34 @@ Portfolio | Rocket.Chip Web Solutions
         </div>
 
         <div class="portfolio-item-info">
+            <div class="portfolio-item-info-about">
+                <h2>About</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, voluptate.</p>
+            </div>
+            <div class="portfolio-item-info-project">
+                <h2>The Project</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident explicabo, odio, sed voluptates adipisci sit omnis repellendus hic veritatis aspernatur nisi quaerat impedit possimus ex ut corporis, quos nobis dolor.</p>
 
+            </div>
+        </div>
+    </div>
+</div>
+<div class="portfolio-container">
+    <div class="portfolio-item">
+        <div class="portfolio-item-img">
+            <a href="http://www.drumcondrafc.com" target="_blank"><img src="/images/drumcondra_logo.png" alt=""></a>
+        </div>
+
+        <div class="portfolio-item-info">
+            <div class="portfolio-item-info-about">
+                <h2>About</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, voluptate.</p>
+            </div>
+            <div class="portfolio-item-info-project">
+                <h2>The Project</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident explicabo, odio, sed voluptates adipisci sit omnis repellendus hic veritatis aspernatur nisi quaerat impedit possimus ex ut corporis, quos nobis dolor.</p>
+
+            </div>
         </div>
     </div>
 </div>
