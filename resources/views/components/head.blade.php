8bf8c95a49ded282eee558978f13d5ecce7307e3
Peter Tiernan <ptiernan@gmail.com>
2019-05-22 13:01:47 +0100

    updating SEO

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -3,6 +3,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="description" content="Welcome to Rocket.Chip Web Solutions. Dublin-based web design and development company who specialise in creating websites for small and medium businesses">
     <meta name="keywords" content="Web design, web development, Dublin-based, affordable, Small business, responsive design, custom design">
+    <meta name="robots" content="index, follow">
     <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700,900" rel="stylesheet">
