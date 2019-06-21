bfe007148a0f56170b02155bc4538e1aa4ffd751
Peter Tiernan <ptiernan@gmail.com>
2019-06-21 13:59:54 +0100

    adding city farm to portfolio

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -10,42 +10,22 @@ Portfolio | Rocket.Chip Web Solutions
 <div class="about-banner">
     <h1>Portfolio</h1>
 </div>
-
-<div class="portfolio-container portfolio-container-1">
-    <div class="portfolio-item portfolio-item-1">
-        <div class="portfolio-item-img">
-            <a href="https://www.larrysdiy.ie" target="_blank"><img src="/images/larryslogo_trans.png" alt="" class="larrys-img"></a>
-        </div>
-
-        <div class="portfolio-item-info">
-            <div class="portfolio-item-info-about">
-                <h2>About</h2>
-                <p>Larry's DIY are a family run hardware and DIY store based in Dublin. They specialise in wood cutting and pride themselves on providing a personal service to their customers.</p>
-            </div>
-            <div class="portfolio-item-info-project">
-                <h2>The Project</h2>
-                <p>Larry's DIY had an existing website which was created using outdated web technologies. The project involved a complete redesign and redevelopment of their site, including creating individual service pages and integration of google maps on their contact page. </p>
-
-            </div>
-        </div>
-    </div>
-</div>
 <div class="portfolio-container portfolio-container-2">
     <div class="portfolio-item">
         <div class="portfolio-item-img">
-            <a href="http://www.drumcondrafc.com" target="_blank"><img src="/images/drumcondra_logo.png" alt="" class="drumcondra-img"></a>
+            <a href="https://www.stannescityfarm.ie" target="_blank"><img src="/images/cityfarm.png" alt="" class="larrys-img"></a>
         </div>
 
         <div class="portfolio-item-info">
             <div class="portfolio-item-info-about">
                 <h2>About</h2>
-                <p>Drumcondra A.F.C are a successful soccer club based on the north side of Dublin. They have teams from under age through to senior league. </p>
+                <p>St. Anne's City Farm is an educational hub that encourages people to come get involved in growing their own food and being in nature.</p>
+                <p>As an awardee of the Google Innovation fund, they launched on the 26th of April 2019 and required a website to engage with the public.</p>
             </div>
             <div class="portfolio-item-info-project">
                 <h2>The Project</h2>
-                <p>Drumcondra A.F.C required a website which provided relevant information to players, parents and managers. The project included the development of a news section, fixtures &amp; results section, and image gallery.</p>
-                <br>
-                <p>The development also involved creating a bespoke administration area where managers can log in to update fixtures, results, and add news items to the site.</p>
+                <p>Rocket.Chip Web Solutions worked closely with St. Anne's City Farm to build a website from the ground up which captured their vision and the range of activities taking place at the farm</p>
+                <p>The website is a custom design, with a unique look and feel. It includes an events calendar and news section which can be easily updated by the staff at St. Anne's City Farm.</p>
 
             </div>
         </div>
@@ -72,4 +52,45 @@ Portfolio | Rocket.Chip Web Solutions
         </div>
     </div>
 </div>
+<div class="portfolio-container portfolio-container-2">
+    <div class="portfolio-item">
+        <div class="portfolio-item-img">
+            <a href="http://www.drumcondrafc.com" target="_blank"><img src="/images/drumcondra_logo.png" alt="" class="drumcondra-img"></a>
+        </div>
+
+        <div class="portfolio-item-info">
+            <div class="portfolio-item-info-about">
+                <h2>About</h2>
+                <p>Drumcondra A.F.C are a successful soccer club based on the north side of Dublin. They have teams from under age through to senior league. </p>
+            </div>
+            <div class="portfolio-item-info-project">
+                <h2>The Project</h2>
+                <p>Drumcondra A.F.C required a website which provided relevant information to players, parents and managers. The project included the development of a news section, fixtures &amp; results section, and image gallery.</p>
+                <br>
+                <p>The development also involved creating a bespoke administration area where managers can log in to update fixtures, results, and add news items to the site.</p>
+
+            </div>
+        </div>
+    </div>
+</div>
+<div class="portfolio-container portfolio-container-1">
+    <div class="portfolio-item portfolio-item-1">
+        <div class="portfolio-item-img">
+            <a href="https://www.larrysdiy.ie" target="_blank"><img src="/images/larryslogo_trans.png" alt="" class="larrys-img"></a>
+        </div>
+
+        <div class="portfolio-item-info">
+            <div class="portfolio-item-info-about">
+                <h2>About</h2>
+                <p>Larry's DIY are a family run hardware and DIY store based in Dublin. They specialise in wood cutting and pride themselves on providing a personal service to their customers.</p>
+            </div>
+            <div class="portfolio-item-info-project">
+                <h2>The Project</h2>
+                <p>Larry's DIY had an existing website which was created using outdated web technologies. The project involved a complete redesign and redevelopment of their site, including creating individual service pages and integration of google maps on their contact page. </p>
+
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
