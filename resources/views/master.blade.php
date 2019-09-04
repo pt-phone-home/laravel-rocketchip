73b54769c0519a9a6f2bd95a0366d8fcbf63ec9b
Peter Tiernan <ptiernan@gmail.com>
2019-09-04 13:36:51 +0100

    adding chat bot

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -27,6 +27,7 @@
   <script>
     AOS.init();
   </script>
+  <script type='text/javascript' data-cfasync='false'>window.purechatApi = { l: [], t: [], on: function () { this.l.push(arguments); } }; (function () { var done = false; var script = document.createElement('script'); script.async = true; script.type = 'text/javascript'; script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript'; document.getElementsByTagName('HEAD').item(0).appendChild(script); script.onreadystatechange = script.onload = function (e) { if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) { var w = new PCWidget({c: 'a4fd48c4-1a4e-42ad-a097-5f29d8b098e7', f: true }); done = true; } }; })();</script>
 
     @yield('scripts')
 
