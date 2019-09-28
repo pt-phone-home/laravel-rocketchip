2baf8f869144265381ada83a845f2205d9e57b77
Peter Tiernan <ptiernan@gmail.com>
2019-09-28 19:51:12 +0100

    working on redesign

--- resources/views/services/webdesign.blade.php
+++ resources/views/services/webdesign.blade.php
@@ -4,9 +4,27 @@
     Website Design & Development
 @endsection
 
-@section('serviceContent')
-Testing
+@section('serviceContentHeader')
+
 @endsection
 
+@section('serviceHeading', 'Website Design & Development')
 
+@section('serviceSubHeading', 'That works for your business')
+
+@section('serviceInfo', 'Work with us to craft a custom website for your business that gets you results.')
+
+
+@section('serviceContent')
+<div class="webdesign-page-section-1-wrapper">
+    <div class="webdesign-page-section-1">
+        <div class="webdesign-page-section-1-left">
+            <h1>Hello</h1>
+        </div>
+        <div class="webdesign-page-section-1-right">
+
+        </div>
+    </div>
+</div>
+@endsection
 
--- resources/views/servicesmaster.blade.php
+++ resources/views/servicesmaster.blade.php
@@ -9,8 +9,24 @@
 <div class="about-banner">
     <h1>@yield('serviceTitle')</h1>
 </div>
+<div class="service-content-header-wrapper">
+    <div class="service-content-header">
+        <div class="service-content-header-left">
+           <div class="webdesign-page-header">
+                <h2 class="webdesign-page-header-heading"> @yield('serviceHeading') </h2>
+                <h3 class="webdesign-page-header-subheading">@yield('serviceSubHeading')</h3>
 
-@include('partials.servicesnav')
+                <p class="webdesign-page-header-info">@yield('serviceInfo')</p>
+
+                <hr>
+             </div>
+        </div>
+        <div class="service-content-header-right">
+            @include('partials.servicesnav')
+        </div>
+    </div>
+</div>
+
 
 @yield('serviceContent')
 @endsection
