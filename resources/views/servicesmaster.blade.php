b8a9796fbfc3d622a20d5dff5fc164666bc6c4d6
Peter Tiernan <ptiernan@gmail.com>
2019-10-07 17:27:43 +0100

    website redesign

--- resources/views/servicesmaster.blade.php
+++ resources/views/servicesmaster.blade.php
@@ -14,11 +14,12 @@
         <div class="service-content-header-left">
            <div class="webdesign-page-header">
                 <h2 class="webdesign-page-header-heading"> @yield('serviceHeading') </h2>
-                <h3 class="webdesign-page-header-subheading">@yield('serviceSubHeading')</h3>
+                {{-- <h3 class="webdesign-page-header-subheading">@yield('serviceSubHeading')</h3> --}}
+                <h3 class="{{ Route::current()->getName() === 'webdesign' || Route::current()->getName() === 'ecommerce' || Route::current()->getName() === 'webapp' || Route::current()->getName() === 'redesign'? 'webdesign-page-header-subheading': 'webdesign-page-header-subheading-alt'}}">@yield('serviceSubHeading')</h3>
 
                 <p class="webdesign-page-header-info">@yield('serviceInfo')</p>
 
-                <hr>
+                <hr class="{{ Route::current()->getName() === 'webdesign' || Route::current()->getName() === 'ecommerce' || Route::current()->getName() === 'webapp' || Route::current()->getName() === 'redesign'? 'webdesign-page-header-hr': 'webdesign-page-header-hr-alt'}}">
              </div>
         </div>
         <div class="service-content-header-right">
