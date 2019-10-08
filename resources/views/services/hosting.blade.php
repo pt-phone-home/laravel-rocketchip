8f2b5e4256803f9bb0642293d85f859b2c1fa7c0
Peter Tiernan <ptiernan@gmail.com>
2019-10-08 11:59:11 +0100

    website redesign

--- resources/views/services/hosting.blade.php
+++ resources/views/services/hosting.blade.php
@@ -20,23 +20,50 @@
 <div class="hosting-page-section-1-wrapper">
     <div class="hosting-page-section-1">
         <div class="hosting-page-section-1-left">
-            <h2 class="hosting-page-section-1-left-heading">Not All Website Hosting is the Same</h2>
-            <p class="hosting-page-section-1-left-info">Search Engine Optimisation or SEO is the process of getting 'traffic' from natural search results on search engines such as Google. This is <strong>not</strong> the same as paid search ads, instead it focuses on the design and presentation of your website to ensure it ranks highly in Google searches.</p>
-            <p class="hosting-page-section-1-left-info">SEO can be carried out on new or existing websites, increasing the likelihood of your business appearing in a user's Google search as they browse for products or services online.</p>
+            <h2 class="hosting-page-section-1-left-heading">World-class Website Hosting</h2>
+            <p class="hosting-page-section-1-left-info">Web hosting is essentially rented server space on a remote computer which stores the files and information for your website. When a customer views your website, they are accessing the information stored at this hosting location. The quality and speed of your website hosting is vital to ensure your customers have a positive experience when visiting your website.</p>
+            <p class="hosting-page-section-1-left-info">Many companies offer inferior hosting packages that reduce the speed and reliability of your website. At Rocket.Chip Web Solutions we offer state-of-the-art cloud hosting powered by <a href="https://www.digitalocean.com">Digital Ocean</a>. Digital Ocean cloud hosting offers our clients the following benefits.</p>
 
 
-            <h2 class="hosting-page-section-1-left-heading">Key Features of SEO</h2>
-            <p class="hosting-page-section-1-left-info"><i class="fal fa-search"></i>Clear and purposeful structuring of website content</p>
-            <p class="hosting-page-section-1-left-info"><i class="fal fa-search"></i>Identifying and implementing appropriate keywords related to a business niche market</p>
-            <p class="hosting-page-section-1-left-info"><i class="fal fa-search"></i>Developing and managing website metadata such as page and image descriptions</p>
-            <p class="hosting-page-section-1-left-info"><i class="fal fa-search"></i>Optimising site performance on all devices, especially mobile</p>
-            <p class="hosting-page-section-1-left-info"><i class="fal fa-search"></i>Building external links and social media profiles</p>
-            <p class="hosting-page-section-1-left-info"><i class="fal fa-search"></i>Maintaining relevant content that potential customers will find interesting and valuable</p>
-            <p class="hosting-page-section-1-left-info"><i class="fal fa-search"></i>Set up and integration of web analytics to monitor website and SEO performance</p>
+            <h2 class="hosting-page-section-1-left-heading">Digital Ocean Cloud Hosting</h2>
+            <p class="hosting-page-section-1-left-info"><i class="fal fa-check-circle"></i><strong>Super-fast</strong> cloud storage - meaning your website will have excellent performance</p>
+            <p class="hosting-page-section-1-left-info"><i class="fal fa-check-circle"></i><strong>Incredibly reliable</strong> - 99.99% uptime, meaning your website will never be 'down'</p>
+            <p class="hosting-page-section-1-left-info"><i class="fal fa-check-circle"></i><strong>Scalable</strong> - if your business grows, your server space can grow with you</p>
+            <p class="hosting-page-section-1-left-info"><i class="fal fa-check-circle"></i><strong>Global servers</strong> - your website will load quickly no matter where in world you are based</p>
+            <p class="hosting-page-section-1-left-info"><i class="fal fa-check-circle"></i><strong>Competitive pricing</strong> - world-class hosting for an afforable monthly fee.</p>
         </div>
         <div class="hosting-page-section-1-right">
 
         </div>
+    </div>
+</div>
+<div class="seo-page-section-2-wrapper">
+    <div class="seo-page-section-2">
+        <a class="seo-page-section-2-button" href="{{ route('contact') }}">Work With Us Today</a>
+    </div>
+</div>
+<div class="hosting-page-section-3-wrapper">
+    <div class="hosting-page-section-3">
+        <div class="hosting-page-section-3-left">
+
+        </div>
+        <div class="hosting-page-section-3-right">
+            <h2 class="hosting-page-section-3-right-heading">Website Maintenance</h2>
+            <p class="hosting-page-section-3-right-info">Our team are experts in SEO for SME's and sole traders in Dublin and Ireland. We provide our clients with top quality search engine optimisation services which improve your visiblity online and increase quality traffic to your website.</p>
+            <p class="hosting-page-section-3-right-info">Our experts conduct a comprehensive SEO review of your site, your competition and your industry. This allows us to tailor our SEO efforts for maximum impact.</p>
+            <p class="hosting-page-section-3-right-info">An SEO review with Rocket.Chip Web Solutions involves:</p>
+            <h2 class="hosting-page-section-3-right-heading">Content Writing that Promotes Your Business</h2>
+            <p class="hosting-page-section-3-right-info"><i class="fal fa-search"></i><strong>On-page SEO</strong> including a complete review of your website content, heading tags, meta data and page descriptions</p>
+            <p class="hosting-page-section-3-right-info"><i class="fal fa-search"></i><strong>Local SEO</strong> including Google My Business Set-up, identification of local <strong>keywords</strong>, and confirmation of your company's contact information</p>
+            <p class="hosting-page-section-3-right-info"><i class="fal fa-search"></i><strong>Competition SEO</strong> including a review of your competitors SEO performance and keword targeting</p>
+            <p class="hosting-page-section-3-right-info"><i class="fal fa-search"></i><strong>Security SEO</strong> - Google takes website security very seriously. Our team conduct a full security audit of your site and take necessary steps to improve problem areas. </p>
+
+        </div>
+    </div>
+</div>
+<div class="seo-page-section-4-wrapper">
+    <div class="seo-page-section-4">
+        <a class="seo-page-section-4-button" href="{{ route('contact') }}">Get Started</a>
     </div>
 </div>
 @endsection
