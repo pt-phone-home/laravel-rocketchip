a7b71d1a5c4af5a4730e0e5b0cfa2fa177f6802c
Peter Tiernan <ptiernan@gmail.com>
2019-10-08 13:59:24 +0100

    website redesign

--- resources/views/services/design.blade.php
+++ resources/views/services/design.blade.php
@@ -1,8 +1,20 @@
 @extends('servicesmaster')
-
-@section('serviceTitle')
-    Graphic Design
+@section('servicePageTitle')
+    Graphic Design Services | Rocket.Chip Web Solutions
 @endsection
+@section('serviceTitle')
+    Graphic Design Services
+@endsection
+
+@section('serviceContentHeader')
+
+@endsection
+
+@section('serviceHeading', 'Graphic Design Services')
+
+@section('serviceSubHeading', 'Striking Design in Print and Online')
+
+@section('serviceInfo', 'All the graphic design services you need to promote your business')
 
 
 
